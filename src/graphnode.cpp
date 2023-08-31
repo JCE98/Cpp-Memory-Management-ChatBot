1b0ec00ca952c73877ccd93dcaec4b3335f40ba7
JCE98 <josiah@raecodesign.com>
2023-08-31 19:11:07 +0000

    tracking source code

---
+++ src/graphnode.cpp
@@ -0,0 +1,64 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+    if(_chatBot != nullptr){
+        delete _chatBot;
+    }
+    //delete _chatBot;    //Add condition for _chatBot existence to avoid segmentation fault (Task 0)
+    //_chatBot = nullptr; //Attempt to resolve program crash on exit (task 0) 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(std::unique_ptr<GraphEdge> edge)
+{
+    _parentEdges.push_back(std::move(edge));
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot & chatbot)
+{
+    //_chatBot = &chatbot;
+    *(_chatBot) = std::move(chatbot);
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(* _chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
